107585b3c98d8dcc0983c16ede2e58aef0590681
David Adamson <dadamson@cs.cmu.edu>
2013-08-23 01:37:46 -0400

    created branch for Applied Machine Learning Fall 2013. Bug-fixes only.

---
+++ src/edu/cmu/side/view/generic/GenericMetricChecklistPanel.java
@@ -0,0 +1,122 @@
+package edu.cmu.side.view.generic;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+
+import se.datadosen.component.RiverLayout;
+import edu.cmu.side.Workbench;
+import edu.cmu.side.model.data.FeatureTable;
+import edu.cmu.side.model.feature.Feature;
+import edu.cmu.side.plugin.FeatureMetricPlugin;
+import edu.cmu.side.view.util.AbstractListPanel;
+import edu.cmu.side.view.util.CheckBoxListEntry;
+import edu.cmu.side.view.util.FastListModel;
+import edu.cmu.side.view.util.SelectPluginList;
+
+public abstract class GenericMetricChecklistPanel<E extends FeatureMetricPlugin> extends AbstractListPanel{
+
+	FastListModel pluginsModel = new FastListModel();
+	SelectPluginList pluginsList = new SelectPluginList();
+
+	FeatureTable localTable;
+	
+	public GenericMetricChecklistPanel(){
+		setLayout(new RiverLayout());
+		pluginsModel = new FastListModel();
+		Map<E, Map<String, Boolean>> evalPlugins = getEvaluationPlugins();
+		ArrayList pluginsToPass = new ArrayList();
+		for(E plug : evalPlugins.keySet()){
+			pluginsToPass.add(plug);
+			Map<String, Boolean> opts = new TreeMap<String, Boolean>();
+			for(Object s : plug.getAvailableEvaluations().keySet()){
+				opts.put(s.toString(), false);
+				CheckBoxListEntry entry = new CheckBoxListEntry(s, false);
+				entry.addItemListener(getCheckboxListener());
+				pluginsToPass.add(entry);					
+			}
+			evalPlugins.put(plug, opts);
+		}
+		pluginsModel.addAll(pluginsToPass.toArray());			
+		pluginsList.setModel(pluginsModel);
+
+		combo.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if(combo.getSelectedItem() != null){
+					setTargetAnnotation(combo.getSelectedItem().toString());					
+				}
+				Workbench.update(GenericMetricChecklistPanel.this);
+			}
+		});
+		//combo.setRenderer(new AbbreviatedComboBoxCellRenderer(30));
+		add("left", new JLabel("Evaluations to Display:"));
+		add("br left", new JLabel("Target:"));
+		add("hfill", combo);
+		describeScroll = new JScrollPane(pluginsList);
+		add("br hfill vfill", describeScroll);
+	}
+
+	public void refreshPanel(FeatureTable table){
+		if(table != localTable){
+			localTable = table;
+			Set<String> keysNew = new TreeSet<String>();
+			if(table != null)
+			{
+				for(String s : table.getLabelArray())
+				{
+					keysNew.add(s);
+				}
+			}
+			Feature.Type activeType = (localTable == null?null: localTable.getClassValueType());
+			Workbench.reloadComboBoxContent(combo, keysNew, (keysNew.size()>0?keysNew.toArray(new String[0])[0]:null));
+			for(int i = 0; i < pluginsModel.getSize(); i++){
+				if(pluginsModel.get(i) instanceof CheckBoxListEntry)
+				{
+
+					CheckBoxListEntry check = ((CheckBoxListEntry)pluginsModel.get(i));
+					String label = check.getValue().toString();
+					Map<E, Map<String, Boolean>> evalPlugins = getEvaluationPlugins();
+					for (E plug : evalPlugins.keySet())
+					{
+						Collection<Feature.Type> types = (Collection<Feature.Type>) plug.getAvailableEvaluations().get(label);
+						
+						if (types != null && activeType != null && !types.contains(activeType))
+						{
+							check.setSelected(false);
+							check.setEnabled(false);
+						}
+						else
+						{
+							check.setEnabled(true);
+						}
+
+					}
+				}
+			}
+//			for(int i = 0; i < pluginsModel.getSize(); i++){
+//				if(pluginsModel.get(i) instanceof CheckBoxListEntry){
+//					CheckBoxListEntry check = ((CheckBoxListEntry)pluginsModel.get(i));
+//				}
+//			}
+			pluginsList.setModel(pluginsModel);
+			revalidate();
+			repaint();
+		}
+	}
+	
+	public abstract ItemListener getCheckboxListener();
+
+	public abstract Map<E, Map<String, Boolean>> getEvaluationPlugins();
+	
+	public abstract void setTargetAnnotation(String s);
+}
---
+++ src/edu/cmu/side/view/predict/PredictActionBar.java
@@ -0,0 +1,169 @@
+package edu.cmu.side.view.predict;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JCheckBox;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+
+import edu.cmu.side.Workbench;
+import edu.cmu.side.control.PredictLabelsControl;
+import edu.cmu.side.model.Recipe;
+import edu.cmu.side.model.RecipeManager.Stage;
+import edu.cmu.side.model.StatusUpdater;
+import edu.cmu.side.model.data.DocumentList;
+import edu.cmu.side.view.generic.ActionBar;
+import edu.cmu.side.view.util.WarningButton;
+
+public class PredictActionBar extends ActionBar
+{
+	JCheckBox showMaxScoreBox = new JCheckBox("Show Predicted Label's Score");
+	JCheckBox showDistsBox = new JCheckBox("Show Label Distribution");
+	JCheckBox overwriteBox = new JCheckBox("Overwrite Columns");
+//	JCheckBox useEvaluationBox = new JCheckBox("Use Model Validation Results");
+	WarningButton warn = new WarningButton();
+
+	Recipe lastRecipe = null;
+	boolean lastValidation = false;
+
+	public PredictActionBar(StatusUpdater update)
+	{
+		super("prediction",Stage.PREDICTION_RESULT, update);
+		actionButton.setIcon(new ImageIcon("toolkits/icons/application_form_edit.png"));
+		actionButton.setText("Predict");
+		actionButton.setEnabled(false);
+		nameLabel.setText("New Column Name:");
+		name.setColumns(10);
+		name.setText("prediction");
+		name.addCaretListener(new CaretListener()
+		{
+			@Override
+			public void caretUpdate(CaretEvent arg0)
+			{
+				checkColumnName();
+			}
+		});
+		
+		overwriteBox.addActionListener(new ActionListener()
+		{
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0)
+			{
+				checkColumnName();
+			}
+		});
+		
+//		useEvaluationBox.setToolTipText("Just use the prediction results from the (cross-validated, etc) model evaluation.");
+		
+		settings.add("left", warn);
+//		settings.add("left", showMaxScoreBox);
+//		settings.add("left", useEvaluationBox);
+		settings.add("left", showDistsBox);
+		settings.add("left", overwriteBox);
+		
+//		useEvaluationBox.addActionListener(new ActionListener()
+//		{
+//			@Override
+//			public void actionPerformed(ActionEvent arg0)
+//			{
+//				String nameSuggestion = getColumnNameSuggestion();
+//				name.setText(nameSuggestion);
+//				//setText!
+//			}
+//			
+//		});
+		
+		actionButton.addActionListener(new ActionListener()
+		{
+
+			@Override
+			public void actionPerformed(ActionEvent arg0)
+			{
+				PredictLabelsControl.executePredictTask(PredictActionBar.this, name.getText(), showMaxScoreBox.isSelected(), showDistsBox.isSelected(), overwriteBox.isSelected(), PredictLabelsControl.shouldUseValidationResults());
+			}
+		});
+		
+	}
+
+	@Override
+	public void startedTask()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void endedTask()
+	{
+		Workbench.update(this);
+		Workbench.update(Stage.DOCUMENT_LIST);
+	}
+	
+	@Override
+	public void refreshPanel()
+	{
+		Recipe trainRecipe = PredictLabelsControl.getHighlightedTrainedModelRecipe();
+		Recipe unlabeledRecipe = PredictLabelsControl.getHighlightedUnlabeledData();
+
+		boolean validation = PredictLabelsControl.shouldUseValidationResults();
+		if(lastRecipe != trainRecipe || lastValidation != validation)
+		{
+			lastRecipe = trainRecipe;
+			lastValidation = validation;
+			name.setText(getColumnNameSuggestion());
+		}
+		checkColumnName();
+		
+		if(trainRecipe != null && unlabeledRecipe != null)
+		{
+			DocumentList evalList = trainRecipe.getTrainingResult().getEvaluationTable().getDocumentList();
+			DocumentList labelList = unlabeledRecipe.getDocumentList();
+			
+			setPredictOnTrain(evalList.equals(labelList));
+		}
+	}
+
+	protected boolean isPredictionPossible()
+	{
+		return PredictLabelsControl.hasHighlightedTrainedModelRecipe() && (PredictLabelsControl.shouldUseValidationResults() || PredictLabelsControl.hasHighlightedUnlabeledData());
+	}
+
+	protected void checkColumnName()
+	{
+		if(name.getText().isEmpty())
+		{
+			warn.setWarning("You need to provide a name for the predicted label.");
+			actionButton.setEnabled(false);
+		}
+		else if(isPredictionPossible() && 
+				PredictLabelsControl.getHighlightedUnlabeledData().getDocumentList().allAnnotations().containsKey(name.getText()))
+		{
+			warn.setWarning("This will over-write an existing column.");
+//			actionButton.setEnabled(overwriteBox.isSelected());
+		}
+		else
+		{
+			warn.clearWarning();
+			actionButton.setEnabled(isPredictionPossible());
+		}
+	}
+	
+	public void setPredictOnTrain(boolean matchingDocs)
+	{
+//		useEvaluationBox.setEnabled(matchingDocs);
+//		useEvaluationBox.setSelected(matchingDocs);
+	}
+
+	protected String getColumnNameSuggestion()
+	{
+		Recipe recipe = PredictLabelsControl.getHighlightedTrainedModelRecipe();
+		String prefix = recipe == null ? "": recipe.getTrainingTable().getAnnotation()+"_";
+		String suffix = PredictLabelsControl.shouldUseValidationResults()?"validation":"prediction";
+		String nameSuggestion = prefix+suffix;
+		return nameSuggestion;
+	}
+
+}
