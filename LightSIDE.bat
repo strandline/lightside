fc5b8031170405ef87b5a942ff71bce4cf67c7df
David Adamson <Mr.Adamson@gmail.com>
2014-04-17 13:27:53 -0400

    updated .bat script to match public distributions

--- LightSIDE.bat
+++ LightSIDE.bat
@@ -3,7 +3,9 @@
 
 set memory=1G
 
-set classpath=bin;lib/*;lib/xstream/*
+set other_args=-XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode
+
+set classpath="bin;plugins/genesis.jar;lib/*;lib/xstream/*"
 
 set wekafiles=wekafiles/packages/chiSquaredAttributeEval/chiSquaredAttributeEval.jar;wekafiles/packages/bayesianLogisticRegression/bayesianLogisticRegression.jar;wekafiles/packages/LibLINEAR/lib/liblinear-1.8.jar;wekafiles/packages/LibLINEAR/LibLINEAR.jar;wekafiles/packages/LibSVM/lib/libsvm.jar;wekafiles/packages/LibSVM/LibSVM.jar
 
@@ -12,5 +14,5 @@ set splash=toolkits/icons/logo.png
 set mainclass=edu.cmu.side.Workbench
 
 ( echo %date% %time%
-  java.exe -Xmx"%memory%" -splash:"%splash%" -classpath "%classpath%";"%wekafiles%" "%mainclass%"
+  java.exe -Xmx"%memory%" %other_args% -splash:"%splash%" -classpath "%classpath%";"%wekafiles%" "%mainclass%"
 ) >>lightside_log.log 2>&1
