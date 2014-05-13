af3ef0f7759981b0e6080a8729ddb20785805166
David Adamson <Mr.Adamson@gmail.com>
2014-05-13 18:43:53 -0400

    updated shell scripts for extraction, training, and prediction.

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-if [ "$#" -lt 1 ] || [ "$#" -gt 1 ] || [ $1 == "-h" ] || [ $1 == "--help" ];
-then
-    echo "Usage: ./predict.sh path/to/trained/model [path/to/unlabeled/data]"
-    echo "Predict labels on new data (using an already-trained model), either from a given CSV or from console input."
-    exit
-fi
+# Usage: scripts/extract.sh {arff|csv|xml} {data-encoding} path/to/template.xml path/to/output/table path/to/data.csv
+# Extracts a new feature table with the same extraction settings as template.xml 
+# (any saved LightSide feature table or model)
+# Feature table can be saved in ARFF, CSV, or LightSide XML formats.
+# Common data encodings are UTF-8, windows-1252, and MacRoman.
+# (Make sure that the text columns and any columns used as features 
+#  have the same names in the new data as they did in the template.)
 
 MAXHEAP="4g"
 OS_ARGS=""
 #OTHER_ARGS=""
 OTHER_ARGS="-XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.awt.headless=true"
 
-MAIN_CLASS="edu.cmu.side.recipe.Predictor"
+MAIN_CLASS="edu.cmu.side.recipe.Extractor"
 
 CLASSPATH="bin:lib/*:lib/xstream/*:wekafiles/packages/chiSquaredAttributeEval/chiSquaredAttributeEval.jar:wekafiles/packages/bayesianLogisticRegression/bayesianLogisticRegression.jar:wekafiles/packages/LibLINEAR/lib/liblinear-1.8.jar:wekafiles/packages/LibLINEAR/LibLINEAR.jar:wekafiles/packages/LibSVM/lib/libsvm.jar:wekafiles/packages/LibSVM/LibSVM.jar:plugins/genesis.jar"
         
